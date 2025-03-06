e4b267b5b2f773bb0a3c85b4e895dad7612db425
Abner Santos <abner.santos@bdsdatasolution.com.br>
2025-03-05 22:42:26 -0300

    add WhatsAppApplication and service configuration for shopping bot functionality

--- src/ListaAgil/ListaAgil/Program.cs
+++ src/ListaAgil/ListaAgil/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var serviceCollection = new ServiceCollection();
+
+Services.ConfigureServices(serviceCollection);
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var app = serviceProvider.GetService<IWhatsAppApplication>();
+app.ShoppingBot("@mercado");
---
+++ src/ListaAgil/ListaAgil/Usings.cs
@@ -0,0 +1,12 @@
+﻿global using ListaAgil.Configurations;
+global using ListaAgil.Domain.Interfaces;
+global using ListaAgil.Application;
+global using ListaAgil.Infrastructure.ExternalServices;
+
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Text;
+global using System.Text.RegularExpressions;
+global using OpenQA.Selenium;
+global using System.Collections.ObjectModel;
+global using OpenQA.Selenium.Chrome;
+global using OpenQA.Selenium.Support.UI;
